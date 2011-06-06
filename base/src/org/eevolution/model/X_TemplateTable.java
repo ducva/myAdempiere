e36668aa7c72fc5d0a780027fc247a1d809ca9c6
U-ducva-PC\ducva <ducva@ducva-PC.(none)>
2011-06-07 00:30:38 +0700

    first

---
+++ base/src/org/eevolution/model/X_TemplateTable.java
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.eevolution.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.model.*;
+
+/** Generated Model for TemplateTable
+ *  @author Adempiere (generated) 
+ *  @version Release 3.6.0LTS - $Id$ */
+public class X_TemplateTable extends PO implements I_TemplateTable, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20110605L;
+
+    /** Standard Constructor */
+    public X_TemplateTable (Properties ctx, int TemplateTable_ID, String trxName)
+    {
+      super (ctx, TemplateTable_ID, trxName);
+      /** if (TemplateTable_ID == 0)
+        {
+        } */
+    }
+
+    /** Load Constructor */
+    public X_TemplateTable (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 7 - System - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_TemplateTable[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+}
